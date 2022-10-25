0f95c0829b10f588b3cb7aa39557dcd7e3cd0a01
Hanwen <hanwen.liu@tum.de>
2022-10-25 20:56:11 +0200

    small update

--- hw01/library.cpp
+++ hw01/library.cpp
@@ -2,6 +2,7 @@
  * Our precious library function, returning
  * the answer to the Ultimate Question of Life, the Universe, and Everything.
  */
-int library_function() {
-    return 1337;
+int library_function()
+{
+    return 42;
 }
