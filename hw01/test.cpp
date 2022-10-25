542da732585f959924ae9e49ad995201af1f631b
Hanwen <hanwen.liu@tum.de>
2022-10-25 22:47:06 +0200

    Update test.cpp

--- hw01/test.cpp
+++ hw01/test.cpp
@@ -1,3 +1,5 @@
+// g++ -std=c++20 -Wall -Wextra -Wl,-rpath . -I "/Users/hanwen/Library/CloudStorage/OneDrive-TUM/TUM/第三学期/C++/gitlab/doctest/doctest/" -o hw01test test.cpp -L . -llibrary
+
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 
