eba705f6699eeaec5babecb47cfc9a5322ea9326
Hanwen <hanwen.liu@tum.de>
2022-10-26 14:01:49 +0200

    tmp

--- hw01/library.cpp
+++
@@ -1,8 +0,0 @@
-/**
- * Our precious library function, returning
- * the answer to the Ultimate Question of Life, the Universe, and Everything.
- */
-int library_function()
-{
-    return 1337;
-}
--- hw01/test.cpp
+++
@@ -1,9 +0,0 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include <doctest/doctest.h>
-
-#include "library.h"
-
-TEST_CASE("testing the library")
-{
-    CHECK(library_function() == 42);
-}
