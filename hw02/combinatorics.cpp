eb6999e875c6a9a196e25836ef345b3ad7a4a2cf
Hanwen <hanwen.liu@tum.de>
2022-11-02 00:31:00 +0100

    update

--- hw02/combinatorics.cpp
+++ hw02/combinatorics.cpp
@@ -1,19 +1,21 @@
 #include "combinatorics.h"
 
-
-uint64_t factorial(uint64_t val) {
-    // TODO
-    return 0;
+uint64_t factorial(uint64_t val)
+{
+    uint64_t result = 1;
+    for (uint64_t i = 0; i < val; i++)
+    {
+        result *= i;
+    }
+    return result;
 }
 
-
-uint64_t permutation(uint64_t val, uint64_t val2) {
-    // TODO
-    return 0;
+uint64_t permutation(uint64_t val, uint64_t val2)
+{
+    return factorial(val) / factorial(val - val2);
 }
 
-
-uint64_t combination(uint64_t val, uint64_t val2) {
-    // TODO
-    return 0;
+uint64_t combination(uint64_t val, uint64_t val2)
+{
+    return factorial(val) / (factorial(val2) * factorial(val - val2));
 }
