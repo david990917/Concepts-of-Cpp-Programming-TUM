26d6a185abe1269c2e233eb9dc7f1fcf1ad7c9c4
Hanwen <hanwen.liu@tum.de>
2022-11-02 08:27:36 +0100

    pruning acc

--- hw02/combinatorics.cpp
+++ hw02/combinatorics.cpp
@@ -8,6 +8,7 @@ uint64_t factorial(uint64_t val)
     {
         return result_save[val];
     }
+
     uint64_t result = 1;
     for (uint64_t i = 1; i <= val; i++)
     {
@@ -24,10 +25,18 @@ uint64_t factorial(uint64_t val)
 
 uint64_t permutation(uint64_t val, uint64_t val2)
 {
+    if (val < val2)
+    {
+        return 0;
+    }
     return factorial(val) / factorial(val - val2);
 }
 
 uint64_t combination(uint64_t val, uint64_t val2)
 {
+    if (val < val2)
+    {
+        return 0;
+    }
     return factorial(val) / (factorial(val2) * factorial(val - val2));
 }
