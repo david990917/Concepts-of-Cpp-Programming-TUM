20ea292ebee440c170fbf7cd64733f0806352145
Hanwen <hanwen.liu@tum.de>
2022-11-02 01:03:25 +0100

    math update

--- hw02/run.cpp
+++ hw02/run.cpp
@@ -3,10 +3,13 @@
 
 #include <iostream>
 
-
-int main() {
+int main()
+{
     // here you can test your functions
+    std::cout << "fact(10)=" << factorial(10) << std::endl;
+    std::cout << "fact(3)=" << factorial(3) << std::endl;
     std::cout << "perm(10, 3)=" << permutation(10, 3) << std::endl;
+    std::cout << "comb(10, 3)=" << combination(10, 3) << std::endl;
 
     return 0;
 }
