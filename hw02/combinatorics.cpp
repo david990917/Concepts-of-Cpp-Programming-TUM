aa5a74372a8e947150de0289f148049113dcf5bc
Hanwen <hanwen.liu@tum.de>
2022-11-02 08:24:44 +0100

    acc version

--- hw02/combinatorics.cpp
+++ hw02/combinatorics.cpp
@@ -1,12 +1,24 @@
 #include "combinatorics.h"
 
+uint64_t result_save[21]{0};
+
 uint64_t factorial(uint64_t val)
 {
+    if (result_save[val] != 0)
+    {
+        return result_save[val];
+    }
     uint64_t result = 1;
     for (uint64_t i = 1; i <= val; i++)
     {
         result *= i;
     }
+
+    if (val <= 20)
+    {
+        result_save[val] = result;
+    }
+
     return result;
 }
 
