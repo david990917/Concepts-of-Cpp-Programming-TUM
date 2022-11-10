255cc1a7ef7a6659f5034ae1b9f4c4ff8dc31437
Hanwen <hanwen.liu@tum.de>
2022-11-10 21:34:13 +0100

    hw03

--- hw03/contact_list.cpp
+++ hw03/contact_list.cpp
@@ -1,7 +1,115 @@
 #include "contact_list.h"
 
+#include <iostream>
 #include <algorithm>
 #include <numeric>
 #include <sstream>
+#include <string>
 
 // TODO create implementation here!
+namespace contact_list
+{
+    /**
+     * Given a contact storage, create a new contact entry by name and number.
+     */
+    bool add(storage &contacts, std::string_view name, number_t number)
+    {
+        std::string sname = std::string(name);
+        if (sname.empty() || std::count(contacts.names.begin(), contacts.names.end(), sname) != 0)
+        {
+            return false;
+        }
+
+        contacts.names.push_back(sname);
+        contacts.numbers.push_back(number);
+        return true;
+    }
+
+    /**
+     * Given a contact storage, how many contacts are currently stored?
+     */
+    size_t size(const storage &contacts)
+    {
+        return contacts.numbers.size();
+    }
+
+    /**
+     * Fetch a contact number from storage given a name.
+     */
+    number_t get_number_by_name(storage &contacts, std::string_view name)
+    {
+        std::string sname = std::string(name);
+        for (unsigned i = 0; i < contacts.names.size(); i++)
+        {
+            if (contacts.names[i] == sname)
+            {
+                return contacts.numbers[i];
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Return a string representing the contact list.
+     */
+    std::string to_string(const storage &contacts)
+    {
+        for (unsigned i = 0; i < contacts.names.size(); i++)
+        {
+            std::cout << contacts.names[i] << " - " << contacts.numbers[i] << "\n";
+        }
+    }
+
+    /**
+     * Remove a contact by name from the contact list.
+     */
+    bool remove(storage &contacts, std::string_view name)
+    {
+        std::string sname = std::string(name);
+        for (unsigned i = 0; i < contacts.names.size(); i++)
+        {
+            if (contacts.names[i] == sname)
+            {
+                contacts.names.erase(contacts.names.begin() + i);
+                contacts.numbers.erase(contacts.numbers.begin() + i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Sort the contact list in-place by name.
+     */
+    void sort(storage &contacts)
+    {
+        std::sort(contacts.names.begin(), contacts.names.end());
+        std::vector<number_t> tmp_numbers;
+        for (unsigned i = 0; i < contacts.names.size(); i++)
+        {
+            for (unsigned j = 0; j < contacts.names.size(); j++)
+            {
+                if (contacts.names[j] == contacts.names[i])
+                {
+                    tmp_numbers.push_back(contacts.numbers[j]);
+                }
+            }
+        }
+        contacts.numbers = tmp_numbers;
+    }
+
+    /**
+     * Fetch a contact name from storage given a number.
+     */
+    std::string get_name_by_number(storage &contacts, number_t number)
+    {
+        for (unsigned int i = 0; i < contacts.numbers.size(); i++)
+        {
+            if (contacts.numbers[i] == number)
+            {
+                return contacts.names[i];
+            }
+        }
+        return "";
+    }
+}
--- hw03/run.cpp
+++ hw03/run.cpp
@@ -3,19 +3,21 @@
 
 #include <iostream>
 
-namespace contact_list {
+namespace contact_list
+{
 
-void test_contact_list() {
-    storage abook;
-    add(abook, "Best Friend", 421234);
-    add(abook, "False Friend", 10032331);
-    std::cout << to_string(abook);
-}
+    void test_contact_list()
+    {
+        storage abook;
+        add(abook, "Best Friend", 421234);
+        add(abook, "False Friend", 10032331);
+        std::cout << to_string(abook);
+    }
 
 } // namespace contact_list
 
-
-int main() {
+int main()
+{
     contact_list::test_contact_list();
 
     return 0;
