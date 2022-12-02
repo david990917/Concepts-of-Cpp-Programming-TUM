6f1a2fc37802d5f948c78bd7de74e5df20367a32
Hanwen <hanwen.liu@tum.de>
2022-12-02 18:35:39 +0100

    top

---
+++ hw05/README.md
@@ -0,0 +1,6 @@
+# hw05 作业总结与讨论
+
+## Step1 - 修正 CMakeLists.txt
+
+重新生成 build 文件夹，需要删除掉根目录下的 cache 文件，然后再重新 cmake ..
+
--- hw05/token.h
+++ hw05/token.h
@@ -7,42 +7,52 @@ namespace sql {
 
 namespace token {
 /// Each token is represented by a struct, this is the bases of our type based token system
-struct Select {};
+struct Select
+{};
 
 /// Token can also carry some information, but this is more to show of the possibilities, we are not
 /// using it here
-struct Identifier {
-  std::string name;
+struct Identifier
+{
+    std::string name;
 };
 
 /*
  * TODO: all the tokens go between here
  */
+struct From
+{};
+struct Comma
+{};
+struct Asterisks
+{};
+struct Semicolon
+{};
+
 /*
  * ... and here
  */
-} // namespace token
+}   // namespace token
 
 /// Simple class representing a token for our simplified SQL select clause. A token be any of the
 /// token types. And at runtime you can query which type is currently stored in the token.
-class Token {
+class Token
+{
 public:
-  /// TODO: Add all types of token to the variant
-  using token_type =
-      std::variant<token::Select, token::Identifier>;
+    /// TODO: Add all types of token to the variant
+    using token_type = std::variant<token::Select, token::Identifier>;
 
-  // Disallow default construction, this doesn't really make sense, what should be a default
-  // token? Maybe Unknown, but we don't have that so just disallow it
-  Token() = delete;
+    // Disallow default construction, this doesn't really make sense, what should be a default
+    // token? Maybe Unknown, but we don't have that so just disallow it
+    Token() = delete;
 
-  // Construct a token from a variant
-  Token(token_type value);
+    // Construct a token from a variant
+    Token(token_type value);
 
-  /// Getter for the underlying variant
-  [[nodiscard]]
-  token_type value() const;
+    /// Getter for the underlying variant
+    [[nodiscard]] token_type value() const;
 
 private:
-  token_type value_;
+    token_type value_;
 };
-} // namespace sql
+}   // namespace sql
