6e2c1cb9185ed7f8d89fafd82aeaa11bc6fd658b
Hanwen <hanwen.liu@tum.de>
2023-02-02 21:32:27 +0100

    hw12

--- hw12/logger.cpp
+++ hw12/logger.cpp
@@ -1,41 +1,45 @@
 #include "logger.h"
 
-#include <ctime>
 #include <chrono>
+#include <ctime>
 
+Logger::Logger(const std::string& filename)
+    : file{}
+{
+    auto cwd = fs::current_path();
 
+    if (fs::exists(filename)) {
+        if (fs::exists(filename + "_old")) {
+            fs::remove(filename + "_old");
+        }
+        fs::rename(filename, filename + "_old");
+    }
 
-Logger::Logger(const std::string &filename) : file{} {
+    file.open(filename);
+    if (not file.is_open()) {
+        throw std::runtime_error("File could not be opened!");
+    }
+
     std::time_t time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-    file << "Commencing logging for directory: "
-         << "TODO: insert current path here.."
-         << std::endl
-         << "Current time is: "
-         << std::ctime(&time)
-         << std::endl;
+    file << "Commencing logging for directory: " << cwd << std::endl
+         << "Current time is: " << std::ctime(&time) << std::endl;
 }
 
-Logger::~Logger() {
+Logger::~Logger()
+{
     // explicit file closing needed in Windows
 }
 
-void Logger::log(const std::string &path, status what) const {
+void Logger::log(const std::string& path, status what) const
+{
     if (not file.is_open()) {
         throw std::runtime_error("File could not be opened!");
     }
 
     switch (what) {
-    case status::added :
-        file << "+ File was added:    " << path << std::endl;
-        break;
-    case status::changed :
-        file << "! File was modified: " << path << std::endl;
-        break;
-    case status::removed :
-        file << "~ File was deleted:  " << path << std::endl;
-        break;
-    default:
-        file << "? Error! Unknown file status detected for: " << path << std::endl;
-        break;
+    case status::added: file << "+ File was added:    " << path << std::endl; break;
+    case status::changed: file << "! File was modified: " << path << std::endl; break;
+    case status::removed: file << "~ File was deleted:  " << path << std::endl; break;
+    default: file << "? Error! Unknown file status detected for: " << path << std::endl; break;
     }
 }
