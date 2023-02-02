2f04db880c81e2d21519091e59226c7abc784b89
Hanwen <hanwen.liu@tum.de>
2023-02-02 20:27:02 +0100

    hw12

--- hw12/logger.cpp
+++ hw12/logger.cpp
@@ -1,41 +1,50 @@
 #include "logger.h"
 
-#include <ctime>
 #include <chrono>
+#include <ctime>
+
+#include <filesystem>
+namespace fs = std::filesystem;
 
 
-
-Logger::Logger(const std::string &filename) : file{} {
+Logger::Logger(const std::string& filename)
+    : file{fs::current_path() / filename}
+    , filename(filename)
+{
     std::time_t time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
     file << "Commencing logging for directory: "
-         << "TODO: insert current path here.."
-         << std::endl
-         << "Current time is: "
-         << std::ctime(&time)
-         << std::endl;
+         << "TODO: insert current path here.." << std::endl
+         << "Current time is: " << std::ctime(&time) << std::endl;
 }
 
-Logger::~Logger() {
+Logger::~Logger()
+{
+    auto dir         = fs::current_path();
+    auto orgfilename = dir / filename;
+    if (fs::exists(orgfilename)) {
+        if (fs::exists(orgfilename + "_old")) {
+            fs::remove(orgfilename + "_old");
+        }
+        fs::rename(orgfilename, orgfilename / "_old");
+    }
+
+    file.open(orgfilename);
+    if (!file.is_open()) {
+        throw std::runtime_error("File could not be opened!");
+    }
     // explicit file closing needed in Windows
 }
 
-void Logger::log(const std::string &path, status what) const {
+void Logger::log(const std::string& path, status what) const
+{
     if (not file.is_open()) {
         throw std::runtime_error("File could not be opened!");
     }
 
     switch (what) {
-    case status::added :
-        file << "+ File was added:    " << path << std::endl;
-        break;
-    case status::changed :
-        file << "! File was modified: " << path << std::endl;
-        break;
-    case status::removed :
-        file << "~ File was deleted:  " << path << std::endl;
-        break;
-    default:
-        file << "? Error! Unknown file status detected for: " << path << std::endl;
-        break;
+    case status::added: file << "+ File was added:    " << path << std::endl; break;
+    case status::changed: file << "! File was modified: " << path << std::endl; break;
+    case status::removed: file << "~ File was deleted:  " << path << std::endl; break;
+    default: file << "? Error! Unknown file status detected for: " << path << std::endl; break;
     }
 }
--- hw12/monitor.cpp
+++ hw12/monitor.cpp
@@ -4,13 +4,11 @@
 
 
 
-FileMonitor::FileMonitor(const std::string &targetpath,
-                         std::chrono::milliseconds interval,
-                         const std::string &logfilename)
+FileMonitor::FileMonitor(const std::string& targetpath, std::chrono::milliseconds interval,
+                         const std::string& logfilename)
     : logger{logfilename}
     , interval{interval}
 {}
 
 // Monitor the targetpath for changes and pass information to the logger in case of differences
-void FileMonitor::start(std::chrono::seconds timeout) {
-}
+void FileMonitor::start(std::chrono::seconds timeout) {}
